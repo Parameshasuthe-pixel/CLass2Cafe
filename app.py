65a48c3ebf90849abc5f0ed0b0d8be23dddc644f
Parameshasuthe-pixel <radhasuthradhasuthav@gmail.com>
2026-06-11 10:07:18 +0530

    Added webhook route fix

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ app.register_blueprint(payment_bp)
 def home():
     return render_template("chatbot.html")
 
+from flask import request
+from twilio.twiml.messaging_response import MessagingResponse
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    incoming_msg = request.form.get("Body")
+
+    resp = MessagingResponse()
+    resp.message(f"You said: {incoming_msg}")
+
+    return str(resp)
 
 with app.app_context():
     db.create_all()
