991e3d9e9939e6357a6597f712b12c7dad59c9e1
Parameshasuthe-pixel <radhasuthradhasuthav@gmail.com>
2026-06-11 18:17:03 +0530

    Remove duplicate webhook

--- routes/chatbot_routes.py
+++ routes/chatbot_routes.py
@@ -1,24 +1,12 @@
 from flask import Blueprint, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-from chatbot import process_message
-
 chatbot_bp = Blueprint("chatbot", __name__)
-
 
 @chatbot_bp.route("/webhook", methods=["POST"])
 def webhook():
 
-    incoming_msg = request.form.get("Body", "")
-    sender = request.form.get("From", "")
-
-    print("MESSAGE:", incoming_msg)
-
-    response_text = process_message(sender, incoming_msg)
-
-    print("RESPONSE:", response_text)
-
     twilio_response = MessagingResponse()
-    twilio_response.message(response_text)
+    twilio_response.message("Hello from Class2Cafe!")
 
-    return str(twilio_response), 200, {"Content-Type": "text/xml"}
+    return str(twilio_response)
