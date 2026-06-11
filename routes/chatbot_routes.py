a304167c08ac17736b3003981746e304bed0af58
Parameshasuthe-pixel <radhasuthradhasuthav@gmail.com>
2026-06-12 00:25:48 +0530

    Added webhook logging

--- routes/chatbot_routes.py
+++ routes/chatbot_routes.py
@@ -5,10 +5,13 @@ chatbot_bp = Blueprint("chatbot", __name__)
 
 @chatbot_bp.route("/webhook", methods=["POST"])
 def webhook():
-    print("WEBHOOK HIT",flush=True)
-    print(request.form,flush=True)
+
+    print("WEBHOOK HIT", flush=True)
+
+    incoming_msg = request.form.get("Body")
+    print("Message:", incoming_msg, flush=True)
 
     twilio_response = MessagingResponse()
-    twilio_response.message("Hello from Class2Cafe!")
+    twilio_response.message("Received: " + str(incoming_msg))
 
     return str(twilio_response)
